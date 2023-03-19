d54769584f7588b249f009286061b22b55d85ed1
Sourav <sapnandu@gmail.com>
2023-03-19 11:32:53 +0530

    CreateCase added

---
+++ CreateCase/Helper/ErrorLogger.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace CreateCase
+{
+    public static class ErrorLogger
+    {
+        public static void LogCustomEvent(string ErrorMessage)
+        {
+            Dictionary<string, object> message = new Dictionary<string, object>();
+            message["Messages"] = ErrorMessage;
+            TelemetryClient teleClient = new TelemetryClient();
+            teleClient.Context.InstrumentationKey = KeyVaultService.getSecret("APPINSIGHTS_INSTRUMENTATIONKEY");
+            var eventTrigger = new EventTelemetry("Info Message");
+            foreach (var d in message)
+            {
+                eventTrigger.Properties.Add(d.Key, d.Value == null ? "" : d.Value.ToString());
+            }
+            teleClient.TrackEvent(eventTrigger);
+        }
+
+        public static void requestPerser(HttpRequestMessage request, HttpResponseMessage ResponsMessage)
+        {
+            MultipartContent multipartContent = (MultipartContent)request.Content;
+            string requestmes = "SendAsync > Request \n ";
+            foreach (var contens in multipartContent)
+            {
+                if (contens.GetType() == typeof(MultipartContent))
+                {
+                    var newcontent = (MultipartContent)contens;
+                    foreach (var ncontens in newcontent)
+                    {
+                        requestmes += ncontens.ReadAsStringAsync().Result;
+                    }
+                }
+                else if (contens.GetType() == typeof(HttpMessageContent))
+                {
+                    requestmes += contens.ReadAsStringAsync().Result;
+                }
+            }
+
+            requestmes += "\n SendAsync > Response " + ResponsMessage.Content.ReadAsStringAsync().Result;
+            LogCustomEvent($"SendAsync : {requestmes}");
+        }
+        
+    }
+}
---
+++ CreateCase/Interface/KeyVaultService.cs
@@ -0,0 +1,30 @@
+﻿namespace CreateCase
+{
+    using System;
+    using System.Runtime.CompilerServices;
+    using Microsoft.Extensions.Configuration;
+    public class KeyVaultService : IKeyVaultService
+    {
+        /// <summary>
+        /// This function reads keys from from config
+        /// </summary>
+        /// <param name="key">This is the key</param>
+        /// <returns>The value of the key from configuration</returns>
+        public string ReadSecret(string key)
+        {
+            var MyConfig = new ConfigurationBuilder().AddJsonFile("data/config/appsettings.json").Build();
+            string AppsettingValue = MyConfig.GetValue<string>(key);
+            return AppsettingValue;
+        }
+
+
+        public static string getSecret(string key)
+        {
+            string AppsettingValue = new KeyVaultService().ReadSecret(key);
+            return AppsettingValue;
+        }
+
+    }
+
+
+}

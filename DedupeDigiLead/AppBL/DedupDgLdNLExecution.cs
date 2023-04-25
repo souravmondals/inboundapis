bd5d0dd01b57f35e369577921e5ba00ed2f3e427
Sourav <sapnandu@gmail.com>
2023-04-25 08:15:30 +0530

    DedupDigiLead Added

---
+++ DedupeDigiLead/AppBL/DedupDgLdNLExecution.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace DedupeDigiLead
+{
+    public class DedupDgLdNLExecution : IDedupDgLdNLExecution
+    {
+
+        private ILoggers _logger;
+        private IQueryParser _queryParser;
+        public string API_Name { set
+            {
+                _logger.API_Name = value;
+            }
+        }
+        public string Input_payload { set {
+                _logger.Input_payload = value;
+            } 
+        }
+
+        private readonly IKeyVaultService _keyVaultService;        
+        
+        private CommonFunction commonFunc;
+
+        public DedupDgLdNLExecution(ILoggers logger, IQueryParser queryParser, IKeyVaultService keyVaultService)
+        {
+                    
+            this._logger = logger;
+            
+            this._keyVaultService = keyVaultService;
+            this._queryParser = queryParser;
+            this.commonFunc = new CommonFunction(queryParser);
+           
+           
+
+        }
+
+
+        public async Task<dynamic> ValidateDedupDgLdNL(dynamic RequestData, string appkey, string type)
+        {
+
+            dynamic ldRtPrm = (type == "NLTR") ? new DedupDgLdNLTRReturn() : new DedupDgLdNLReturn();
+            try
+            {
+                string LeadID = RequestData.LeadID;
+                if (!string.IsNullOrEmpty(appkey) && appkey != "" && checkappkey(appkey, "DedupDgLdNLappkey"))
+                {
+                    if (!string.IsNullOrEmpty(LeadID) && LeadID != "")
+                    {
+
+                        ldRtPrm = await this.getDedupDgLdNLStatus(RequestData, type);
+
+                    }
+                    else
+                    {
+                        this._logger.LogInformation("ValidateFtchDgLdSts", "Input parameters are incorrect");
+                        ldRtPrm.ReturnCode = "CRM-ERROR-102";
+                        ldRtPrm.Message = OutputMSG.Incorrect_Input;
+                    }
+                }
+                else
+                {
+                    this._logger.LogInformation("ValidateFtchDgLdSts", "Input parameters are incorrect");
+                    ldRtPrm.ReturnCode = "CRM-ERROR-102";
+                    ldRtPrm.Message = OutputMSG.Incorrect_Input;
+                }
+
+                return ldRtPrm;
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogError("ValidateFtchDgLdSts", ex.Message);
+                throw ex;
+            }
+
+        }
+
+        
+
+
+        public bool checkappkey(string appkey, string APIKey)
+        {
+            if (this._keyVaultService.ReadSecret(APIKey) == appkey)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public async Task<dynamic> getDedupDgLdNLStatus(dynamic RequestData, string type)
+        {
+            dynamic ldRtPrm = (type == "NLTR") ? new DedupDgLdNLTRReturn() : new DedupDgLdNLReturn();
+            return new DedupDgLdNLReturn();
+        }
+
+
+    }
+}
---
+++ DedupeDigiLead/Interface/IDedupDgLdNLExecution.cs
@@ -0,0 +1,11 @@
+﻿namespace DedupeDigiLead
+{
+    public interface IDedupDgLdNLExecution
+    {
+        public string API_Name { set; }
+        public string Input_payload { set; }
+        
+        public Task<dynamic> ValidateDedupDgLdNL(dynamic CaseData, string appkey, string type);
+        public Task<dynamic> getDedupDgLdNLStatus(dynamic RequestData, string type);        
+    }
+}

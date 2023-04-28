3ce8b40c9226160f841f9b4c9d486d0b8edb8063
Sourav <sapnandu@gmail.com>
2023-04-28 19:13:55 +0530

    Unit test added

---
+++ CRMConnect/Interface/IKeyVaultService.cs
@@ -0,0 +1,14 @@
+﻿namespace CRMConnect
+{
+    public interface IKeyVaultService
+    {
+        /// <summary>
+        /// This function reads keys from from config
+        /// </summary>
+        /// <param name="key">This is the key</param>
+        /// <returns>The value of the key from configuration</returns>
+        public string ReadSecret(string key);
+    }
+
+
+}
---
+++ CRMConnect/Interface/ILoggers.cs
@@ -0,0 +1,10 @@
+﻿namespace CRMConnect
+{
+    public interface ILoggers
+    {
+        public string API_Name { get; set; }
+        public string Input_payload { get; set; }
+        public void LogInformation(string FunctionName, string InfoMessage);
+        public void LogError(string FunctionName, string ErrorMessage);
+    }
+}
